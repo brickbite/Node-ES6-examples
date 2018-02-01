14cd04c7b0b554a294d9985c59979ca909d2ba63
brickbite <jeffreylee0116@gmail.com>
2018-02-01 14:52:36 -0800

    working on problem

--- array-dimensionality.js
+++ array-dimensionality.js
@@ -1,3 +1,10 @@
-export function arrayDim(arr) {
-  return 1;
+export function arrayDim(arr, maxDims = 0) {
+  if (arr.isArray()) {
+    maxDims++;
+    arr.forEach((ele) => {
+      maxDims = arrayDim(ele, dims);
+    });
+  }
+  
+  return maxDims;
 };
