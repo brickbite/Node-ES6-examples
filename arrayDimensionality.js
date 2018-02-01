f2080566bfa9ce4be525b3241420e4361ad5ae9b
brickbite <jeffreylee0116@gmail.com>
2018-01-31 20:01:15 -0800

    initial commit

---
+++ arrayDimensionality.js
@@ -0,0 +1,4 @@
+export default function arrayDim() {
+
+  return 1;
+}
---
+++ arrayDimensionality.test.js
@@ -0,0 +1,19 @@
+import arrayDim from './arrayDimensionality';
+
+test('Empty array is of dimension 1', () => {
+  expect(arrayDim([]).toBe(1));
+});
+
+test('Array with no objects is dimension 1', () => {
+  expect(arrayDim([1, 'a']).toBe(1));
+});
+
+test('Array containing arrays is of dimension 2', () => {
+  expect(arrayDim([[], [], []]).toBe(2));
+});
+
+test('Array containing an array is of dimension 2', () => {
+  expect(arrayDim(['a', 1, 0, []]).toBe(2));
+});
+
+
