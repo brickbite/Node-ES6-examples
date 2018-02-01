d5357f6acd43550bd2bc6103b8ab04862da57264
brickbite <jeffreylee0116@gmail.com>
2018-02-01 14:29:23 -0800

    added files for findDistance

---
+++ find-distance.js
@@ -0,0 +1,3 @@
+export function findDistance(origin, destination, departure) {
+  return 1;
+};
---
+++ find-distance.test.js
@@ -0,0 +1,9 @@
+import { findDistance } from './find-distance';
+
+test('returns miles', () => {
+  expect(findDistance()).toBe(1);
+});
+
+test('returns correct number of miles', () => {
+  expect(findDistance()).toBe(1);
+});
