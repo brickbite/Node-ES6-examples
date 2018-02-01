e971b2d6b9a390f87abdf9290c289ab0ec2590f8
brickbite <jeffreylee0116@gmail.com>
2018-02-01 14:15:20 -0800

    updated test descriptions

--- arrayDimensionality.test.js
+++ arrayDimensionality.test.js
@@ -8,11 +8,11 @@ test('Array with no objects is dimension 1', () => {
   expect(arrayDim([1, 'a'])).toBe(1);
 });
 
-test('Array containing arrays is of dimension 2', () => {
+test('Array containing all empty arrays is of dimension 2', () => {
   expect(arrayDim([[], [], []])).toBe(2);
 });
 
-test('Array containing an array is of dimension 2', () => {
+test('Array containing any number of arrays is of dimension 2', () => {
   expect(arrayDim(['a', 1, 0, []])).toBe(2);
 });
 
