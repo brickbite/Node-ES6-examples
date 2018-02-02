0ac4105ebd2a87ad923959f9fccebc7ab83c4bdf
brickbite <jeffreylee0116@gmail.com>
2018-02-01 16:56:33 -0800

    working on findDistance

--- find-distance.js
+++ find-distance.js
@@ -6,11 +6,13 @@ const getAsync = promisify(https.get);
 
 export async function findDistance(origin, destination, departure) {
   try {
-    const response = await getAsync(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&key=${process.env.API_KEY}`)
-    
+    const response = await getAsync({
+      hostname: `localhost`,
+      path: `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=${process.env.API_KEY}`});
+
     console.log(response);
 
   } catch (err) {
-    console.log(`Error in request: ${err}`)
+    console.log(`Error in request: `, err.headers);
   }
 };
--- find-distance.test.js
+++ find-distance.test.js
@@ -4,6 +4,6 @@ test('returns miles', () => {
   expect(findDistance()).toBe(1);
 });
 
-test('returns correct number of miles', () => {
-  expect(findDistance()).toBe(1);
-});
+// test('returns correct number of miles', () => {
+//   expect(findDistance()).toBe(1);
+// });
