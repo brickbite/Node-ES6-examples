e8e3cdbd1b94bb67fdd5d2ffbf70f2064e442f5c
brickbite <jeffreylee0116@gmail.com>
2018-02-01 16:17:17 -0800

    find-links working

--- find-links.js
+++ find-links.js
@@ -1,10 +1,52 @@
 const { promisify } = require('util');
 const fs = require('fs');
+const path = require('path');
 const readDirAsync = promisify(fs.readdir);
+const readFileAsync = promisify(fs.readFile);
+const statAsync = promisify(fs.stat);
 
-export async function findLinks(dir, link) {
-  const filenames = [];
-  const directories = {name: '', files: [], folders: []};
-  console.log(await readDirAsync(__dirname));
-  return 1;
+export async function findLinks(dir = __dirname, link) {
+  let filepaths = [];
+
+  try {
+    await statAsync(dir);
+  } catch(err) {
+    console.log(`Not a directory`);
+    return;
+  }
+
+  // console.log(`Currentdir: ${dir}`);
+  const dirContent = await readDirAsync(dir);
+
+  // console.log(`dirContent is: ${dirContent}`);
+  for (let i = 0; i < dirContent.length; i++) {
+    try {
+
+      const fileInfo = await statAsync(`${dir}/${dirContent[i]}`);
+      if (fileInfo.isFile() && dirContent[i].includes('.html')) {
+        // console.log(`html file found, reading.. ${dir}/${dirContent[i]}`);
+
+        try {
+          let file = await readFileAsync(path.join(`${dir}/${dirContent[i]}`));
+          if (file.includes(link)) {
+            filepaths.push(`${dir}/${dirContent[i]}`);
+          }
+        } catch(err) {
+          console.log(`Error reading file`, err);
+          return;
+        }
+
+      } else if (fileInfo.isDirectory()) {
+        filepaths = filepaths.concat(await findLinks(`${dir}/${dirContent[i]}`, link));
+      }
+
+    } catch (err) {
+      console.log(`Error getting file info`, err);
+      return;
+    }
+  }
+
+  // filepaths.length > 1 ? console.log(filepaths) : null;
+  
+  return filepaths;
 };
--- find-links.test.js
+++ find-links.test.js
@@ -1,13 +1,19 @@
 import { findLinks } from './find-links';
 
-test('Finds a link inside a file', async () => {
-  expect(await findLinks()).toBe(1);
+test('Valid dirname: returns an array', async () => {
+  const result = await findLinks(`${__dirname}/website`, 'badsite');
+  expect(typeof result).toBe('object');
+  expect(Array.isArray.call(null, result)).toBe(true);
 });
 
-test('Finds multiple links inside a file', async () => {
-  expect(await findLinks()).toBe(1);
+test('Valid dirname: returns an array', async () => {
+  const result = await findLinks(`${__dirname}/website`, 'badsite');
+  expect(result).toEqual([ './website/main.html',
+  './website/next/3/asdf.html',
+  './website/next/something.html',
+  './website/second.html' ]);
 });
 
-test('Finds links from multiple files', async () => {
-  expect(await findLinks()).toBe(1);
+test('Invalid dirname: returns undefined', async () => {
+  expect(await findLinks(`someFakeDirectory/website`)).toBeUndefined();
 });
