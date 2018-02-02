30dce9a527a24dccdf1f550f9ee0bc4fdf609cc7
brickbite <jeffreylee0116@gmail.com>
2018-02-01 16:44:04 -0800

    added error handling

--- find-distance.js
+++ find-distance.js
@@ -5,5 +5,12 @@ const https = require('https');
 const getAsync = promisify(https.get);
 
 export async function findDistance(origin, destination, departure) {
-  return 1;
+  try {
+    const response = await getAsync(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&key=${process.env.API_KEY}`)
+    
+    console.log(response);
+
+  } catch (err) {
+    console.log(`Error in request: ${err}`)
+  }
 };
