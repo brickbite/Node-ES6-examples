6fe0ee5f48640d7d531fd732a4ab89f3bbf994c2
brickbite <jeffreylee0116@gmail.com>
2018-02-04 17:59:24 -0800

    added done for async testing

--- find-links.test.js
+++ find-links.test.js
@@ -1,20 +1,23 @@
 import { findLinks } from './find-links';
 
-test('Valid dirname: returns an array', async () => {
+test('Valid dirname: returns an array', async (done) => {
   const result = await findLinks(`${__dirname}/website`, 'badsite');
   expect(typeof result).toBe('object');
   expect(Array.isArray.call(null, result)).toBe(true);
+  done();
 });
 
-test('Valid dirname: returns an array', async () => {
+test('Valid dirname: returns an array', async (done) => {
   const result = await findLinks(`${__dirname}/website`, 'badsite');
   // added sort for result: async function can get results in different order
   expect(result.sort()).toEqual([`${__dirname}/website/badsite1.html`,
   `${__dirname}/website/next/another/badsite4.html`,
   `${__dirname}/website/next/badsite3.html`,
   `${__dirname}/website/badsite2.html`].sort());
+  done();
 });
 
-test('Invalid dirname: returns undefined', async () => {
+test('Invalid dirname: returns undefined', async (done) => {
   expect(await findLinks(`someFakeDirectory/website`)).toBeUndefined();
+  done();
 });
