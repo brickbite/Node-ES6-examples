b67b5b6de35635e164f5936d5bea2a06ed2045a6
brickbite <jeffreylee0116@gmail.com>
2018-02-04 16:53:48 -0800

    renamed test html files and edited test accordingly

--- find-links.test.js
+++ find-links.test.js
@@ -8,10 +8,11 @@ test('Valid dirname: returns an array', async () => {
 
 test('Valid dirname: returns an array', async () => {
   const result = await findLinks(`${__dirname}/website`, 'badsite');
-  expect(result).toEqual([ './website/main.html',
-  './website/next/3/asdf.html',
-  './website/next/something.html',
-  './website/second.html' ]);
+  // added sort for result: async function can get results in different order
+  expect(result.sort()).toEqual([ './website/badsite1.html',
+  './website/next/another/badsite4.html',
+  './website/next/badsite3.html',
+  './website/badsite2.html'].sort());
 });
 
 test('Invalid dirname: returns undefined', async () => {
